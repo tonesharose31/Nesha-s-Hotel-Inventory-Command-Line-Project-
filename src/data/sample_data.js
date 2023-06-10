cec605d7884da90e84599c6c2cea7cc966f14412
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-06-10 03:32:13 -0400

    some progress

---
+++ src/data/sample_data.js
@@ -0,0 +1,32 @@
+const  { faker }  = require("@faker-js/faker")
+
+let myHotels = [
+{
+hotelNames = faker.name.findName(),
+roomPrice = faker.finance.currencySymbol().faker.number.binary(),
+availableRooms =faker.boolean(),
+roomTypes= faker.science.chemicalElement(),
+
+}
+return myHotels 
+
+// console.log(hotelNames),
+// console.log(availableRooms),
+// console.log(roomPrice),
+// console.log(roomTypes),
+
+// {
+//     hotelNames = 
+//     availableRooms =
+//     roomTypes =
+//     roomPrice =
+
+//     {hotelNames = 
+//         availableRooms =
+//         roomTypes =
+//         roomPrice =
+
+//     },
+
+}]
+  
